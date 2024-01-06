1517ad3b01ba40df88c613d6c3dd65c9a950a0aa
Alexander Petree <apetree1001@email.phoenix.edu>
2024-01-06 12:47:04 -0800

    Update __init__.py

--- src/Tabs/__init__.py
+++ src/Tabs/__init__.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env python
-#
-# File: __init__.py
-# by @BitK_
-#
-from OptionsTab import OptionsTab
-from ProgramsTab import ProgramsTab
-
-
-__all__ = ["OptionsTab", "ProgramsTab"]
+#!/usr/bin/env python # 
+# File: __init__.py # by @BitK_
+# modified dist @Apetree100122 
+from OptionsTab 
+import OptionsTab from ProgramsTab import
+ProgramsTab__all__ = 
+["OptionsTab", "ProgramsTab"]
